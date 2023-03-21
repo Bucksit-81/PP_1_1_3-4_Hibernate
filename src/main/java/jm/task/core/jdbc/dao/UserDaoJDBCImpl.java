9036720471f1e62b8b4fdf470fb3d74b8ba78387
Bucksit-81 <5229290@gmail.com>
2023-03-22 02:55:37 +0300

    What's wrong?

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,98 +2,30 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
-
+public class UserDaoJDBCImpl extends Util implements UserDao {
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS mydbtest.mydbtest" +
-                    "(id mediumint not null auto_increment," +
-                    "name VARCHAR(50)," +
-                    "lastname VARCHAR(50)," +
-                    "age tinyint," +
-                    "PRIMARY KEY (id))");
-            System.out.println("Таблица создана");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS mydbtest.mydbtest");
-            System.out.println("Таблица удалена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO mydbtest.mydbtest(name, lastname, age) VALUES(?,?,?)";
-        try (Connection connection = Util.getConnection(); PreparedStatement preparedStatement =
-                connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("Пользователь (имя, прозвище, возраст) - " + name + " " + lastName +  " " + age + " "
-                    + " добавлен в " +
-                    "базу данных");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public void removeUserById(long id) {
-        try {Connection connection = Util.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM mydbtest.mydbtest" +
-                    " WHERE id = ?");
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<User> getAllUsers() {
-        List<User> allUser = new ArrayList<>();
-        try {Connection connection = Util.getConnection();
-            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM mydbtest.mydbtest");
-            while (resultSet.next()) {
-                long id = resultSet.getLong("id");
-                String name = resultSet.getString("name");
-                String lastName = resultSet.getString("lastName");
-                byte age = resultSet.getByte("age");
-                User user = new User(name, lastName, age);
-                user.setId(id);
-                allUser.add(user);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return allUser;
+        return null;
     }
 
     public void cleanUsersTable() {
-        String sql = "TRUNCATE mydbtest.mydbtest";
-        try (Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-            System.out.println("Таблица очищена");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
-
